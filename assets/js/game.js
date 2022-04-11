f6f6cc450df0c7d4f22e5781068fa8e74c8beb0c
Andrey Rudinskiy <drerudin@gmail.com>
2022-04-10 19:59:28 -0700

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,14 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -18,15 +17,15 @@ var fight = function() {
 
   // Log a resulting message to the console so we know that it worked.
     console.log(
-        playerName + " attached" + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        playerName + " attached" + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
     );
 
     // check enemy's health
     if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died!");
+        window.alert(enemyNames + " has died!");
     }
     else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        window.alert(enemyNames + " still has " + enemyHealth + " health left.");
     }
 
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
@@ -34,7 +33,7 @@ var fight = function() {
 
   // Log a resulting message to the console so we know that it worked.
   console.log(
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+      enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
 
       if (playerHealth <= 0) {
           window.alert(playerName + " has died!");
@@ -45,4 +44,7 @@ var fight = function() {
   
 };
 
-fight();
+// fight();
+for(var i = 0; i< enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
