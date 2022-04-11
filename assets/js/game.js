fd0dd5309375c6be2daa181f3e2e50cc739419c6
Andrey Rudinskiy <drerudin@gmail.com>
2022-04-11 16:46:46 -0700

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -76,6 +76,15 @@ for (var i = 0; i < enemyNames.length; i++) {
     // debugger;
 
     fight(pickedEnemyName);
+    if (i < enemyNames.length - 1) {
+        shop();
+    }
+    if (playerHealth > 0 && i < enemyNames.length - 1) {
+        shop();
+    }
+    if (storeConfirm) {
+        shop();
+    }
     }
     else{
         window.alert('You have lost your robot in battle! Game Over!');
@@ -100,7 +109,45 @@ if (playAgainConfirm) {
 else {
     window.alert("Thank you for playing Robot Gladiators! Come back soon!");
 }
-}
+};
+
+var shop = function() {
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store?  Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
+    switch (shopOptionPrompt) {
+        case "refill":
+            if (playerMoney >= 7) {
+              window.alert("Refilling player's health by 20 for 7 dollars.");
+          
+              // increase health and decrease money
+              playerHealth = playerHealth + 20;
+              playerMoney = playerMoney - 7;
+            }
+            else {
+              window.alert("You don't have enough money!");
+            }
+          
+            break;
+          case "upgrade":
+            if (playerMoney >= 7) {
+              window.alert("Upgrading player's attack by 6 for 7 dollars.");
+          
+             // increase attack and decrease money
+              playerAttack = playerAttack + 6;
+              playerMoney = playerMoney - 7;
+            }
+            else {
+              window.alert("You don't have enough money!");
+            }
+          
+            break;
+        }
+};
+
+    
+
+
 
 startGame();
 
